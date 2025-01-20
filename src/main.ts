09c0fe0f75287fab4908f0f30a3de5c456089c04
Philip Mayrhofer <philip.mayrhofer@yahoo.de>
2025-01-20 16:24:05 +0100

    feat: improve ui

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,59 @@
 import './style.css'
-import { Game, GameLevel } from "./game/game.ts";
+import {Game} from "./game/game.ts";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="app">
-    <div class="launch-screen">
-        <button data-level="Level_1">Level 1</button>
-        <button data-level="Level_2">Level 2</button>
+<!--    <div class="level-selection">-->
+<!--        <button data-level="Level_1">Level 1</button>-->
+<!--        <button data-level="Level_2">Level 2</button>-->
+<!--    </div>-->
+    <div class="screen-container">
+            <div id="launchScreen" class="launch-screen">
+                <div class="game-screen">
+                </div>
+                <span class="launch-screen-text">PRESS SPACE TO START</span>
+            </div>
+            <div class="game-canvas-container" style="display: none">
+                <canvas id="gameCanvas" height="640" width="640"></canvas>
+            </div>
     </div>
-    <canvas id="gameCanvas" height="640" width="640"></canvas>
 <!--    <div id="gameObjectives"></div>-->
 </div>
 `
-const game = new Game();
+let game: Game | undefined = undefined;
+const launchScreen = document.getElementById('launchScreen')! as HTMLDivElement;
+const gameCanvas = document.getElementsByClassName('game-canvas-container')[0]! as HTMLCanvasElement;
 
-document.querySelector('.launch-screen')!.addEventListener('click', (event) => {
-    const target = event.target as HTMLElement;
-    console.log(target)
+// document.querySelector('.level-selection')!.addEventListener('click', (event) => {
+//     const target = event.target as HTMLElement;
+//     console.log(target)
+//
+//     if (target.tagName === 'BUTTON' && target.dataset.level) {
+//         const level = target.dataset.level as keyof typeof GameLevel;
+//         console.log(level)
+//
+//         if (GameLevel[level]) {
+//             startLevel(GameLevel[level]);
+//         } else {
+//             console.error(`Invalid game level: ${level}`);
+//         }
+//     }
+// });
 
-    if (target.tagName === 'BUTTON' && target.dataset.level) {
-        const level = target.dataset.level as keyof typeof GameLevel;
-        console.log(level)
+document.addEventListener('keydown', handleSpacePress);
 
-        if (GameLevel[level]) {
-            startLevel(GameLevel[level]);
-        } else {
-            console.error(`Invalid game level: ${level}`);
-        }
+function handleSpacePress(event: KeyboardEvent) {
+    if (event.key === ' ') {
+        launchScreen.style.display = 'none';
+        gameCanvas.style.display = 'block';
+        document.removeEventListener('keydown', handleSpacePress); // Remove the listener
+        game = new Game();
     }
-});
-
-function startLevel(level: GameLevel) {
-    game.setLevel(level);
 }
+
+// function startLevel(level: GameLevel) {
+//     if (game) {
+//         game.setLevel(level);
+//     }
+// }
 
